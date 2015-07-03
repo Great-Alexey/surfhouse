ed886a8cc2a3fe1e39175462dc1c1e24b743b1f4
Great-Alexey <alexeyvax1990@gmail.com>
2015-07-03 18:03:08 +0300

    added accordion widget to menu

---
+++ js/main.js
@@ -0,0 +1,8 @@
+
+
+$('#accordion').accordion({
+        collapsible: true,
+        active: false
+        });
+
+//$(".imgCard img").draggable(); благодаря этому мы увидели, что jquery ui подключена и работает
