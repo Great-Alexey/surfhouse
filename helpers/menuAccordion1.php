2a85ed48232d6d47530902baa1eadc0e6ec7f8e5
Great-Alexey <alexeyvax1990@gmail.com>
2015-07-10 10:58:42 +0300

    nazvanie sohranennih izmeneniy

---
+++ helpers/menuAccordion1.php
@@ -0,0 +1,31 @@
+
+
+                        <a href="javascript:void(0)" id="menu2">CATEGORY OPTIONS</a>
+                        <nav id="accordion2"> 
+                            <a href="javascript:void(0)">MEN</a>
+                            <div>
+                              <a href="javascript:void(0)">ABOUT1</a>
+                              <a href="javascript:void(0)">ABOUT2</a>    
+                            </div>
+                            <a href="javascript:void(0)">WOMAN</a>    
+                            <div>
+                                <a href="javascript:void(0)">APPAREL1</a>
+                                <a href="javascript:void(0)">APPAREL2</a>
+                                <a href="javascript:void(0)">APPAREL2</a>
+                                <a href="javascript:void(0)">APPAREL2</a>
+                            </div>
+                            <a href="javascript:void(0)">KIDS</a>
+                            <div>
+                                <a href="javascript:void(0)">SURF APPAREL1</a>
+                                <a href="javascript:void(0)">SURF APPAREL2</a>
+                            </div>
+                            <a href="javascript:void(0)">WETSUITS</a>
+                            <div>
+                                <a href="javascript:void(0)">WINDSURF1</a>
+                            </div>
+                            <a href="javascript:void(0)">OUTERWEAR</a>
+                            <div>
+                                <a href="javascript:void(0)">KITESURF1</a>
+                                <a href="javascript:void(0)">KITESURF2</a>
+                            </div>
+                        </nav>
